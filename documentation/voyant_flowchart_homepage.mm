a6ac3a8cd402e05e5d6d872b3e4928ecfc810e36
Johannes Vogl <johannes.vogl@stud.uni-regensburg.de>
2015-03-04 14:57:21 +0100

    Documenation + stuff

---
+++ documentation/voyant_flowchart_homepage.mm
@@ -0,0 +1,203 @@
+<map version="freeplane 1.3.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="Voyant Homepage" ID="ID_1723255651" CREATED="1283093380553" MODIFIED="1421230589932"><hook NAME="MapStyle">
+    <properties show_icon_for_attributes="true"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node">
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right">
+<stylenode LOCALIZED_TEXT="default" MAX_WIDTH="600" COLOR="#000000" STYLE="as_parent">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.note"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="9"/>
+<node TEXT="Fenstermen&#xfc;" POSITION="right" ID="ID_1158129951" CREATED="1421231576619" MODIFIED="1421231584036">
+<edge COLOR="#007c00"/>
+<node TEXT="Options" ID="ID_1486763028" CREATED="1421229340977" MODIFIED="1421231576631">
+<node TEXT="x-Button (schlie&#xdf;en)" ID="ID_708636578" CREATED="1421229380193" MODIFIED="1421229430987"/>
+<node TEXT="Input Format (Dropdown)" ID="ID_950829391" CREATED="1421229388362" MODIFIED="1421229429397">
+<node TEXT="Leave blank for auto-detect" ID="ID_194320252" CREATED="1421229397992" MODIFIED="1421229406631"/>
+<node TEXT="XML" ID="ID_1968325432" CREATED="1421229407251" MODIFIED="1421229410470"/>
+<node TEXT="RSS2" ID="ID_84554488" CREATED="1421229410993" MODIFIED="1421229414145"/>
+<node TEXT="TEI" ID="ID_237807502" CREATED="1421229414489" MODIFIED="1421229416075"/>
+</node>
+<node TEXT="XPath to Content (Textfeld)" ID="ID_1132634173" CREATED="1421229436297" MODIFIED="1421229450343"/>
+<node TEXT="XPath to Author (Textfeld)" ID="ID_214105199" CREATED="1421229450581" MODIFIED="1421229460635"/>
+<node TEXT="XPath to Title (Textfeld)" ID="ID_1619408979" CREATED="1421229462923" MODIFIED="1421229468816"/>
+<node TEXT="XPath to Documents (Textfeld)" ID="ID_551084041" CREATED="1421229470747" MODIFIED="1421229476009"/>
+<node TEXT="right-to-left text direction (Checkbox)" ID="ID_971495631" CREATED="1421229477802" MODIFIED="1421229492643"/>
+<node TEXT="OK (Button)" ID="ID_1945831493" CREATED="1421229497179" MODIFIED="1421229525813">
+<font ITALIC="false"/>
+</node>
+<node TEXT="Cancel (Button)" ID="ID_340613155" CREATED="1421229500673" MODIFIED="1421229529467">
+<font ITALIC="false"/>
+</node>
+<node TEXT="Defaults (Button)" ID="ID_458230432" CREATED="1421229502330" MODIFIED="1421229530606">
+<font ITALIC="false"/>
+</node>
+</node>
+<node TEXT="Export" ID="ID_1907742705" CREATED="1421229352811" MODIFIED="1421231576633">
+<node TEXT="a URL for this tool and current data" ID="ID_1061953885" CREATED="1421229548465" MODIFIED="1421229981714">
+<hook NAME="FirstGroupNode"/>
+</node>
+<node TEXT="an HTML button for this tool and current data" ID="ID_1002353869" CREATED="1421229552862" MODIFIED="1421229552862"/>
+<node TEXT="an HTML snippet to embed this tool and current data" ID="ID_498623771" CREATED="1421229557613" MODIFIED="1421229557613"/>
+<node TEXT="a bibliographic citation for this tool" ID="ID_1489285474" CREATED="1421229561134" MODIFIED="1421229561134"/>
+<node TEXT="Textfeld zum kopieren" ID="ID_1858477367" CREATED="1421229981709" MODIFIED="1421229986074">
+<hook NAME="SummaryNode"/>
+<font ITALIC="true"/>
+<node TEXT="x-Button (schlie&#xdf;en)" ID="ID_899961354" CREATED="1421230018739" MODIFIED="1421230028805"/>
+<node TEXT="OK" ID="ID_282902953" CREATED="1421230005807" MODIFIED="1421230007107"/>
+</node>
+<node TEXT="a URL for a different tool/skin and current data" ID="ID_1874310058" CREATED="1421229565823" MODIFIED="1421229565823">
+<node TEXT="Tool (Dropdown)" ID="ID_113408212" CREATED="1421229643434" MODIFIED="1421229964517">
+<hook NAME="FirstGroupNode"/>
+</node>
+<node TEXT="Skin (Dropdown)" ID="ID_652144279" CREATED="1421229661197" MODIFIED="1421229666495"/>
+<node TEXT="Textfeld zum kopieren" ID="ID_804867218" CREATED="1421229964516" MODIFIED="1421229972290">
+<hook NAME="SummaryNode"/>
+<font ITALIC="true"/>
+<node TEXT="x-Button (schlie&#xdf;en)" ID="ID_1117268795" CREATED="1421230032243" MODIFIED="1421230033555"/>
+<node TEXT="OK" ID="ID_1974630380" CREATED="1421230010854" MODIFIED="1421230011858"/>
+</node>
+</node>
+<node TEXT="OK" ID="ID_1074065275" CREATED="1421229584926" MODIFIED="1421230552949"/>
+<node TEXT="Cancel" ID="ID_1925543783" CREATED="1421229588339" MODIFIED="1421229589368"/>
+</node>
+<node TEXT="Maximize (new window)" ID="ID_200949916" CREATED="1421229354712" MODIFIED="1421231576635">
+<node TEXT="&#xf6;ffnet neuen Tab mit Homepage" ID="ID_800103371" CREATED="1421230050867" MODIFIED="1421230065963">
+<font ITALIC="true"/>
+</node>
+</node>
+<node TEXT="Help" ID="ID_75779378" CREATED="1421229359763" MODIFIED="1421231576649">
+<node TEXT="x-Button (schlie&#xdf;en)" ID="ID_1029559493" CREATED="1421230018739" MODIFIED="1421230028805"/>
+<node TEXT="OK" ID="ID_1852370498" CREATED="1421230005807" MODIFIED="1421230007107"/>
+</node>
+</node>
+<node TEXT="Textfeld" POSITION="right" ID="ID_1787146114" CREATED="1421230086814" MODIFIED="1421230091652">
+<edge COLOR="#00ffff"/>
+<font ITALIC="true"/>
+</node>
+<node TEXT="REVEAL" POSITION="right" ID="ID_880981273" CREATED="1421230092751" MODIFIED="1421230711258">
+<edge COLOR="#ffff00"/>
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#000000" WIDTH="2" TRANSPARENCY="80" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_1167161446" STARTINCLINATION="58;-8;" ENDINCLINATION="-25;-126;" STARTARROW="NONE" ENDARROW="DEFAULT"/>
+</node>
+<node TEXT="Analyse-Seite" LOCALIZED_STYLE_REF="defaultstyle.floating" POSITION="right" ID="ID_1167161446" CREATED="1421230585765" MODIFIED="1421230726449" HGAP="960" VSHIFT="190">
+<hook NAME="FreeNode"/>
+</node>
+<node TEXT="Upload" POSITION="right" ID="ID_794215614" CREATED="1421230098165" MODIFIED="1421230101141">
+<edge COLOR="#7c0000"/>
+<node TEXT="Textfeld bef&#xfc;llt" ID="ID_720482935" CREATED="1421230134794" MODIFIED="1421230145280">
+<node TEXT="Fehlermeldung (Textfeld ignorieren?)" ID="ID_1884010945" CREATED="1421230156698" MODIFIED="1421230174682">
+<font ITALIC="true"/>
+<node TEXT="x-Button (schlie&#xdf;en)" ID="ID_1034423654" CREATED="1421230194641" MODIFIED="1421230196596"/>
+<node TEXT="Yes" ID="ID_323734153" CREATED="1421230178722" MODIFIED="1421230717226">
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#000000" WIDTH="2" TRANSPARENCY="80" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_1288331205" STARTINCLINATION="-119;-50;" ENDINCLINATION="-214;-128;" STARTARROW="NONE" ENDARROW="DEFAULT"/>
+</node>
+<node TEXT="No" ID="ID_1667089030" CREATED="1421230180075" MODIFIED="1421230452450">
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#000000" WIDTH="2" TRANSPARENCY="80" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_1034423654" STARTINCLINATION="1;19;" ENDINCLINATION="-1;68;" STARTARROW="NONE" ENDARROW="DEFAULT"/>
+</node>
+</node>
+</node>
+<node TEXT="Textfeld leer" ID="ID_1288331205" CREATED="1421230149873" MODIFIED="1421230152616">
+<node TEXT="x-Button (schlie&#xdf;en)" ID="ID_1760307279" CREATED="1421230286826" MODIFIED="1421230288166"/>
+<node TEXT="Add" ID="ID_1742135017" CREATED="1421230257988" MODIFIED="1421230262893"/>
+<node ID="ID_1479496034" CREATED="1421230263807" MODIFIED="1421230697705"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Reveal <i>(nur bei bereits hinzugef&#252;gten Dateien)</i>
+    </p>
+  </body>
+</html>
+
+</richcontent>
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#000000" WIDTH="2" TRANSPARENCY="80" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_1167161446" STARTINCLINATION="341;9;" ENDINCLINATION="-67;213;" STARTARROW="NONE" ENDARROW="DEFAULT"/>
+</node>
+<node ID="ID_1403591730" CREATED="1421230295663" MODIFIED="1421230317208"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Remove All <i>(nur bei bereits hinzugef&#252;gten Dateien)</i>
+    </p>
+  </body>
+</html>
+
+</richcontent>
+</node>
+<node ID="ID_1159814131" CREATED="1421230357924" MODIFIED="1421230377912"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Dokument entfernen <i>(bei hinzugef&#252;gten Dokumenten)</i>
+    </p>
+  </body>
+</html>
+
+</richcontent>
+</node>
+</node>
+</node>
+<node TEXT="Open" POSITION="right" ID="ID_840495179" CREATED="1421230102655" MODIFIED="1421230105250">
+<edge COLOR="#00007c"/>
+<node TEXT="x-Button (schlie&#xdf;en)" ID="ID_1934786718" CREATED="1421230286826" MODIFIED="1421230288166"/>
+<node TEXT="Open: (Dropdown)" ID="ID_1486521633" CREATED="1421230509078" MODIFIED="1421230517630"/>
+<node TEXT="OK" ID="ID_685366901" CREATED="1421229584926" MODIFIED="1421230706651">
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#000000" WIDTH="2" TRANSPARENCY="80" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_1167161446" STARTINCLINATION="447;0;" ENDINCLINATION="113;211;" STARTARROW="NONE" ENDARROW="DEFAULT"/>
+</node>
+<node TEXT="Cancel" ID="ID_1717069531" CREATED="1421229588339" MODIFIED="1421229589368"/>
+</node>
+</node>
+</map>
