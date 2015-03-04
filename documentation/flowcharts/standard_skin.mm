a6e374edbcb158a2119d83711a3fb59daafd32aa
pinkdroyd <ben@inrane.de>
2015-03-04 15:10:37 +0100

    cleaned up flowcharts

---
+++ documentation/flowcharts/standard_skin.mm
@@ -0,0 +1,145 @@
+<map version="freeplane 1.3.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="Voyant Analyseseite" ID="ID_1723255651" CREATED="1283093380553" MODIFIED="1421232404824"><hook NAME="MapStyle" zoom="0.909">
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node">
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right">
+<stylenode LOCALIZED_TEXT="default" MAX_WIDTH="600" COLOR="#000000" STYLE="as_parent">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.note"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="12"/>
+<node TEXT="Globales Men&#xfc;" POSITION="right" ID="ID_950849036" CREATED="1421232835706" MODIFIED="1421232841699">
+<edge COLOR="#007c7c"/>
+<node TEXT="Export" ID="ID_683080766" CREATED="1421232848251" MODIFIED="1421232875989">
+<font ITALIC="true"/>
+<hook NAME="FirstGroupNode"/>
+</node>
+<node TEXT="Help" ID="ID_313184795" CREATED="1421232854989" MODIFIED="1421232866182">
+<font ITALIC="true"/>
+</node>
+<node TEXT="siehe Fenstermen&#xfc;" ID="ID_370674710" CREATED="1421232875987" MODIFIED="1421232882454">
+<hook NAME="SummaryNode"/>
+<font ITALIC="true"/>
+</node>
+</node>
+<node TEXT="Cirrus" POSITION="right" ID="ID_187005348" CREATED="1421232411441" MODIFIED="1421232413314">
+<edge COLOR="#ff0000"/>
+<node TEXT="Fenstermen&#xfc;" ID="ID_1572089051" CREATED="1421232531141" MODIFIED="1421232537772">
+<font ITALIC="true"/>
+<node TEXT="Stop Word List (Dropdown)" ID="ID_1969349889" CREATED="1421232565139" MODIFIED="1421232577508"/>
+<node TEXT="Edit Stop Words" ID="ID_1985996574" CREATED="1421232583105" MODIFIED="1421232586923"/>
+<node TEXT="Apply Stop Words Globally (Checkbox)" ID="ID_582361738" CREATED="1421232591584" MODIFIED="1421232601339"/>
+<node TEXT="Search (Textfeld)" ID="ID_149613064" CREATED="1421232603315" MODIFIED="1421232611100"/>
+<node TEXT="OK" ID="ID_1098216438" CREATED="1421232622785" MODIFIED="1421232623643"/>
+<node TEXT="Cancel" ID="ID_1172551041" CREATED="1421232625672" MODIFIED="1421232626570"/>
+<node TEXT="Defaults" ID="ID_499354586" CREATED="1421232626821" MODIFIED="1421232629546"/>
+</node>
+</node>
+<node TEXT="Summary" POSITION="right" ID="ID_693305354" CREATED="1421232416434" MODIFIED="1421232418045">
+<edge COLOR="#0000ff"/>
+<node TEXT="Fenstermen&#xfc;" ID="ID_835568690" CREATED="1421232531141" MODIFIED="1421232537772">
+<font ITALIC="true"/>
+<node TEXT="Stop Word List (Dropdown)" ID="ID_962017448" CREATED="1421232565139" MODIFIED="1421232577508"/>
+<node TEXT="Edit Stop Words" ID="ID_1756934544" CREATED="1421232583105" MODIFIED="1421232586923"/>
+<node TEXT="Apply Stop Words Globally (Checkbox)" ID="ID_1261464414" CREATED="1421232591584" MODIFIED="1421232601339"/>
+<node TEXT="OK" ID="ID_1350100702" CREATED="1421232622785" MODIFIED="1421232623643"/>
+<node TEXT="Cancel" ID="ID_1854796511" CREATED="1421232625672" MODIFIED="1421232626570"/>
+<node TEXT="Defaults" ID="ID_676157771" CREATED="1421232626821" MODIFIED="1421232629546"/>
+</node>
+</node>
+<node TEXT="Words in the Entire Corpus" POSITION="right" ID="ID_1071828068" CREATED="1421232426631" MODIFIED="1421232435120">
+<edge COLOR="#00ff00"/>
+<node TEXT="Fenstermen&#xfc;" ID="ID_104635937" CREATED="1421232531141" MODIFIED="1421232537772">
+<font ITALIC="true"/>
+<node TEXT="Stop Word List (Dropdown)" ID="ID_362689481" CREATED="1421232565139" MODIFIED="1421232577508"/>
+<node TEXT="Edit Stop Words" ID="ID_1295940037" CREATED="1421232583105" MODIFIED="1421232586923"/>
+<node TEXT="Apply Stop Words Globally (Checkbox)" ID="ID_1364378357" CREATED="1421232591584" MODIFIED="1421232601339"/>
+<node TEXT="Comparison Corpus (Dropdown)" ID="ID_1558836460" CREATED="1421232681976" MODIFIED="1421232685649"/>
+<node TEXT="Minimum Z-Score (Dropdown)" ID="ID_1135574923" CREATED="1421232688614" MODIFIED="1421232696775"/>
+<node TEXT="OK" ID="ID_1410472484" CREATED="1421232622785" MODIFIED="1421232623643"/>
+<node TEXT="Cancel" ID="ID_669518311" CREATED="1421232625672" MODIFIED="1421232626570"/>
+<node TEXT="Defaults" ID="ID_1203419333" CREATED="1421232626821" MODIFIED="1421232629546"/>
+</node>
+</node>
+<node TEXT="Corpus Reader" POSITION="right" ID="ID_1940278573" CREATED="1421232436670" MODIFIED="1421232439274">
+<edge COLOR="#ff00ff"/>
+<node TEXT="Fenstermen&#xfc;" ID="ID_923298628" CREATED="1421232531141" MODIFIED="1421232537772">
+<font ITALIC="true"/>
+<node TEXT="right-to-left text direction (Checkbox)" ID="ID_1876092874" CREATED="1421232710329" MODIFIED="1421232715588"/>
+<node TEXT="OK" ID="ID_417852860" CREATED="1421232721636" MODIFIED="1421232725225"/>
+<node TEXT="Cancel" ID="ID_1747094849" CREATED="1421232727223" MODIFIED="1421232729220"/>
+<node TEXT="Defaults" ID="ID_1364538194" CREATED="1421232729455" MODIFIED="1421232730588"/>
+</node>
+</node>
+<node TEXT="Corpus" POSITION="right" ID="ID_34060398" CREATED="1421232751000" MODIFIED="1421232753438">
+<edge COLOR="#7c007c"/>
+<node TEXT="Fenstermen&#xfc; (keine Optionen verf&#xfc;gbar)" ID="ID_1696018694" CREATED="1421232531141" MODIFIED="1421232770202">
+<font ITALIC="true"/>
+</node>
+</node>
+<node TEXT="Word Trends" POSITION="right" ID="ID_583761111" CREATED="1421232445045" MODIFIED="1421232452243">
+<edge COLOR="#ffff00"/>
+<node TEXT="Fenstermen&#xfc; (keine Optionen verf&#xfc;gbar)" ID="ID_1398549698" CREATED="1421232531141" MODIFIED="1421232770202">
+<font ITALIC="true"/>
+</node>
+</node>
+<node TEXT="Keywords in Context" POSITION="right" ID="ID_1252953021" CREATED="1421232454588" MODIFIED="1421232458361">
+<edge COLOR="#7c0000"/>
+<node TEXT="Fenstermen&#xfc; (keine Optionen verf&#xfc;gbar)" ID="ID_845173916" CREATED="1421232531141" MODIFIED="1421232770202">
+<font ITALIC="true"/>
+</node>
+</node>
+<node TEXT="Words in Document" POSITION="right" ID="ID_1730128894" CREATED="1421232460058" MODIFIED="1421232463551">
+<edge COLOR="#00007c"/>
+<node TEXT="Fenstermen&#xfc;" ID="ID_251950646" CREATED="1421232531141" MODIFIED="1421232537772">
+<font ITALIC="true"/>
+<node TEXT="Stop Word List (Dropdown)" ID="ID_884936939" CREATED="1421232565139" MODIFIED="1421232577508"/>
+<node TEXT="Edit Stop Words" ID="ID_764884381" CREATED="1421232583105" MODIFIED="1421232586923"/>
+<node TEXT="Apply Stop Words Globally (Checkbox)" ID="ID_1419476680" CREATED="1421232591584" MODIFIED="1421232601339"/>
+<node TEXT="Minimum Z-Score (Dropdown)" ID="ID_77770173" CREATED="1421232688614" MODIFIED="1421232696775"/>
+</node>
+</node>
+</node>
+</map>
---
+++ documentation/flowcharts/window_menu_example.mm
@@ -0,0 +1,110 @@
+<map version="freeplane 1.3.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="Voyant Fenstermen&#xfc;" ID="ID_1723255651" CREATED="1283093380553" MODIFIED="1421232321827"><hook NAME="MapStyle">
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node">
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right">
+<stylenode LOCALIZED_TEXT="default" MAX_WIDTH="600" COLOR="#000000" STYLE="as_parent">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.note"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="4"/>
+<node TEXT="Options" POSITION="right" ID="ID_1486763028" CREATED="1421229340977" MODIFIED="1421231624733">
+<edge COLOR="#ff0000"/>
+<node TEXT="x-Button (schlie&#xdf;en)" ID="ID_708636578" CREATED="1421229380193" MODIFIED="1421229430987"/>
+<node TEXT="Fenster-spezifische Men&#xfc;punkte" ID="ID_1602757791" CREATED="1421232352594" MODIFIED="1421232368029">
+<font ITALIC="true"/>
+</node>
+<node TEXT="OK (Button)" ID="ID_1945831493" CREATED="1421229497179" MODIFIED="1421229525813">
+<font ITALIC="false"/>
+</node>
+<node TEXT="Cancel (Button)" ID="ID_340613155" CREATED="1421229500673" MODIFIED="1421229529467">
+<font ITALIC="false"/>
+</node>
+<node TEXT="Defaults (Button)" ID="ID_458230432" CREATED="1421229502330" MODIFIED="1421229530606">
+<font ITALIC="false"/>
+</node>
+</node>
+<node TEXT="Export" POSITION="right" ID="ID_1907742705" CREATED="1421229352811" MODIFIED="1421231624737">
+<edge COLOR="#0000ff"/>
+<node TEXT="a URL for this tool and current data" ID="ID_1061953885" CREATED="1421229548465" MODIFIED="1421229981714">
+<hook NAME="FirstGroupNode"/>
+</node>
+<node TEXT="an HTML button for this tool and current data" ID="ID_1002353869" CREATED="1421229552862" MODIFIED="1421229552862"/>
+<node TEXT="an HTML snippet to embed this tool and current data" ID="ID_498623771" CREATED="1421229557613" MODIFIED="1421229557613"/>
+<node TEXT="a bibliographic citation for this tool" ID="ID_1489285474" CREATED="1421229561134" MODIFIED="1421229561134"/>
+<node TEXT="Textfeld zum kopieren" ID="ID_1858477367" CREATED="1421229981709" MODIFIED="1421229986074">
+<hook NAME="SummaryNode"/>
+<font ITALIC="true"/>
+<node TEXT="x-Button (schlie&#xdf;en)" ID="ID_899961354" CREATED="1421230018739" MODIFIED="1421230028805"/>
+<node TEXT="OK" ID="ID_282902953" CREATED="1421230005807" MODIFIED="1421230007107"/>
+</node>
+<node TEXT="a URL for a different tool/skin and current data" ID="ID_1874310058" CREATED="1421229565823" MODIFIED="1421229565823">
+<node TEXT="Tool (Dropdown)" ID="ID_113408212" CREATED="1421229643434" MODIFIED="1421229964517">
+<hook NAME="FirstGroupNode"/>
+</node>
+<node TEXT="Skin (Dropdown)" ID="ID_652144279" CREATED="1421229661197" MODIFIED="1421229666495"/>
+<node TEXT="Textfeld zum kopieren" ID="ID_804867218" CREATED="1421229964516" MODIFIED="1421229972290">
+<hook NAME="SummaryNode"/>
+<font ITALIC="true"/>
+<node TEXT="x-Button (schlie&#xdf;en)" ID="ID_1117268795" CREATED="1421230032243" MODIFIED="1421230033555"/>
+<node TEXT="OK" ID="ID_1974630380" CREATED="1421230010854" MODIFIED="1421230011858"/>
+</node>
+</node>
+<node TEXT="OK" ID="ID_1074065275" CREATED="1421229584926" MODIFIED="1421230552949"/>
+<node TEXT="Cancel" ID="ID_1925543783" CREATED="1421229588339" MODIFIED="1421229589368"/>
+</node>
+<node TEXT="Maximize (new window)" POSITION="right" ID="ID_200949916" CREATED="1421229354712" MODIFIED="1421231624738">
+<edge COLOR="#00ff00"/>
+<node TEXT="&#xf6;ffnet neuen Tab mit Homepage" ID="ID_800103371" CREATED="1421230050867" MODIFIED="1421230065963">
+<font ITALIC="true"/>
+</node>
+</node>
+<node TEXT="Help" POSITION="right" ID="ID_75779378" CREATED="1421229359763" MODIFIED="1421231624741">
+<edge COLOR="#ff00ff"/>
+<node TEXT="x-Button (schlie&#xdf;en)" ID="ID_1029559493" CREATED="1421230018739" MODIFIED="1421230028805"/>
+<node TEXT="OK" ID="ID_1852370498" CREATED="1421230005807" MODIFIED="1421230007107"/>
+</node>
+</node>
+</map>
