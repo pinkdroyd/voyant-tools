0d5901bb82e3ae0d12b352e7473d2b4e9a89f967
Dominik <domii.bauer@gmail.com>
2015-03-05 13:46:22 +0100

    Livequery eingebunden, Iframe vergrößert, Button zum umschalten hinzugefügt

--- Iframe-Test/src/cirrus.js
+++ Iframe-Test/src/cirrus.js
@@ -1,14 +1,22 @@
 console.log("cirrus.js aufgerufen");
 
-/*document.getElementById('ifrm').onload = function() {
-	setTimeout(10000);
-	console.log("hi");
-	console.log(document.getElementById('footer'));
-}*/
-
-var url = "http://voyant-tools.org/tool/Cirrus/?input=http://digitalhumanities.org:8080/dhq/vol/3/3/000067.xml"
+var url = "http://voyant-tools.org/tool/Cirrus/?input=http://digitalhumanities.org:8080/dhq/vol/3/3/000067.xml";
 $('#ifrm').attr('src', url);
 
-$('#ifrm').load(function(){
-    console.log("loaded");
+
+
+document.getElementById('ifrm').onload= function() {
+        console.log("onload");
+};
+
+$( "#change-button" ).click(function() {
+	var url = "http://voyant-tools.org/tool/Bubblelines/?input=http://digitalhumanities.org:8080/dhq/vol/3/3/000067.xml"
+  	$('#ifrm').attr('src', url);
 });
+
+
+
+
+
+
+
