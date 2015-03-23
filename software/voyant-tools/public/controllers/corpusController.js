23e4783ddbf0ad5b624facf267b8ca077728ed0f
fabihoot <fabian.huth@gmx.net>
2015-03-23 18:43:02 +0100

    dropbox save file

--- software/voyant-tools/public/controllers/corpusController.js
+++ software/voyant-tools/public/controllers/corpusController.js
@@ -1,42 +1,26 @@
 Voyant.CorpusController = (function() {
 	var that = {};
-	var client = null;
 	init = function() {
 		console.log("init CorpusController");
-			
-			client = new Dropbox.Client({ key: 'b671y7l5vwfj9l3' });
- 		// Try to complete OAuth flow.
-        client.authenticate({ interactive: false }, function (error, client) {
-            if (error) {
-                alert('Error: ' + error);
-            } else {
-            	
-            }
-        });
 		initButton();
 	};
 
-	function initButton(){
+	function initButton(){	
 
 	var fileInput = $('#files');
 	var uploadButton = $('#upload');	
 	
 	uploadButton.on('click', function() { 
-        
-            doHelloWorld();
-        
+	var	client = new Dropbox.Client({ key: 'b671y7l5vwfj9l3' });
+	client.authenticate(function () {
+    client.writeFile('hello.txt', 'Hello, World!', function () {
+        alert('File written!');
+    });
+});
+           
 	});
-}
-	function doHelloWorld() {
-            client.writeFile('hello.txt', 'Hello, World!', function (error) {
-                if (error) {
-                    alert('Error: ' + error);
-                } else {
-                    alert('File written successfully!');
-                }
-            });
-        }
- 
+	}
+	
 	that.init = init;
 
 
