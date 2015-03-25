1bb9b0d5cb33af305507dacbbf884b8eef1873d7
Dominik <domii.bauer@gmail.com>
2015-03-25 12:01:29 +0100

    Alle Analyze- und Visualize-Tools eingebunden

--- software/voyant-tools/public/javascripts/View.js
+++ software/voyant-tools/public/javascripts/View.js
@@ -5,7 +5,6 @@ Voyant.View = (function() {
 		console.log("init View");
 		appendSidebar(); 
 		appendContent(); 
-		//appendTool("Cirrus");
 	},
 
 	appendSidebar = function () {
@@ -18,7 +17,9 @@ Voyant.View = (function() {
 
 	setupSidebarListener = function () {	
 		setupMainmenuListener(); 
-		setupSubmenuListener(); 
+		setupCorpusListener(); 
+		setupAnalzyeListener(); 
+		setupVisualizeListener(); 
 	},
 
 	setupHover = function () {
@@ -54,22 +55,18 @@ Voyant.View = (function() {
 		    /*Click für Analyze-Button*/
 		    case "sidebar-analyze-button":
 		    	if($("#sidebar-ctfg-button").css("display") == "none") {
-		    		$("#sidebar-summary-button").css("display", "block");
-		        	$("#sidebar-ctfg-button").css("display", "block");
+		    		showAnalyzeSubitems(); 
 		        } else {
-		        	$("#sidebar-summary-button").css("display", "none");
-		        	$("#sidebar-ctfg-button").css("display", "none");
+		        	hideAnalyzeSubitems();
 		        }
 		        break; 
 
 		    /*Click für Visualize-Button*/
 		    case "sidebar-visualize-button":
 		    	if($("#sidebar-cirrus-button").css("display") == "none") {
-		        	$("#sidebar-cirrus-button").css("display", "block");
-		        	$("#sidebar-bubblelines-button").css("display", "block");
+		        	showVisualizeSubitems();
 		        } else {
-		        	$("#sidebar-cirrus-button").css("display", "none");
-		        	$("#sidebar-bubblelines-button").css("display", "none");
+		        	hideVisualizeSubitems();
 		        }
 		        break; 
 
@@ -95,7 +92,8 @@ Voyant.View = (function() {
 		});
 	},
 
-	setupSubmenuListener = function () {
+	//Listener für Untermenüpunkte Corpus
+	setupCorpusListener = function () {
 		$(".sidebar-subitem").click(function(e) {
 		var clickedElement = e.target.id;
 
@@ -111,6 +109,21 @@ Voyant.View = (function() {
 				$("#sidebar-settings-button").css("background-color", "#B02130");
 				appendSettings(); 
 				break; 
+			}
+		});
+	},
+
+	//Listener für Untermenüpunkte Analyze
+	setupAnalzyeListener = function () {
+		$(".sidebar-subitem").click(function(e) {
+		var clickedElement = e.target.id;
+
+		switch(clickedElement) {
+			case "sidebar-corpusgrid-button":
+				unselectMenuItems();
+				$("#sidebar-corpusgrid-button").css("background-color", "#B02130");
+				appendTool("CorpusGrid");
+				break;
 
 			case "sidebar-summary-button":
 				unselectMenuItems();
@@ -124,23 +137,131 @@ Voyant.View = (function() {
 				appendTool("CorpusTypeFrequenciesGrid");
 				break; 
 
+			case "sidebar-dtcfg-button":
+				unselectMenuItems();
+				$("#sidebar-dtcfg-button").css("background-color", "#B02130");
+				appendTool("DocumentTypeCollocateFrequenciesGrid");
+				break; 
+
+			case "sidebar-dtfg-button":
+				unselectMenuItems();
+				$("#sidebar-dtfg-button").css("background-color", "#B02130");
+				appendTool("DocumentTypeFrequenciesGrid");
+				break; 
+
+			case "sidebar-dtkg-button":
+				unselectMenuItems();
+				$("#sidebar-dtkg-button").css("background-color", "#B02130");
+				appendTool("DocumentTypeKwicsGrid");
+				break; 
+			}
+		});
+	}, 
+
+	//Listener für Untermenüpunkte Visualize
+	setupVisualizeListener = function () {
+		$(".sidebar-subitem").click(function(e) {
+		var clickedElement = e.target.id;
+
+		switch(clickedElement) {
+			case "sidebar-bubblelines-button":
+				unselectMenuItems();
+				$("#sidebar-bubblelines-button").css("background-color", "#B02130");
+				appendTool("Bubblelines"); 
+				break; 
+
 			case "sidebar-cirrus-button":
 				unselectMenuItems();
 				$("#sidebar-cirrus-button").css("background-color", "#B02130");
 				appendTool("Cirrus"); 
 				break; 
 
-			case "sidebar-bubblelines-button":
+			case "sidebar-knots-button":
 				unselectMenuItems();
-				$("#sidebar-bubblelines-button").css("background-color", "#B02130");
-				appendTool("Bubblelines"); 
+				$("#sidebar-knots-button").css("background-color", "#B02130");
+				appendTool("Knots"); 
+				break; 
+
+			case "sidebar-lava-button":
+				unselectMenuItems();
+				$("#sidebar-lava-button").css("background-color", "#B02130");
+				appendTool("Lava"); 
+				break; 
+
+			case "sidebar-links-button":
+				unselectMenuItems();
+				$("#sidebar-links-button").css("background-color", "#B02130");
+				appendTool("Links"); 
+				break; 
+
+			case "sidebar-mandala-button":
+				unselectMenuItems();
+				$("#sidebar-mandala-button").css("background-color", "#B02130");
+				appendTool("Mandala"); 
+				break; 
+
+			case "sidebar-rezoviz-button":
+				unselectMenuItems();
+				$("#sidebar-rezoviz-button").css("background-color", "#B02130");
+				appendTool("RezoViz"); 
+				break; 
+
+			case "sidebar-scatterplot-button":
+				unselectMenuItems();
+				$("#sidebar-scatterplot-button").css("background-color", "#B02130");
+				appendTool("ScatterPlot"); 
+				break; 
+
+			case "sidebar-termometer-button":
+				unselectMenuItems();
+				$("#sidebar-termometer-button").css("background-color", "#B02130");
+				appendTool("Termometer"); 
+				break; 
+
+			case "sidebar-termsradio-button":
+				unselectMenuItems();
+				$("#sidebar-termsradio-button").css("background-color", "#B02130");
+				appendTool("TermsRadio"); 
+				break; 	
+
+			case "sidebar-tfc-button":
+				unselectMenuItems();
+				$("#sidebar-tfc-button").css("background-color", "#B02130");
+				appendTool("TypeFrequenciesChart"); 
+				break; 
+				
+			case "sidebar-wcf-button":
+				unselectMenuItems();
+				$("#sidebar-wcf-button").css("background-color", "#B02130");
+				appendTool("WordCountFountain"); 
 				break; 
 			}
 		});
+	}, 
+
+	showAnalyzeSubitems = function () {
+		$(".analyze-subitems").css("display", "block");
+	}, 
+
+	hideAnalyzeSubitems = function () {
+		$(".analyze-subitems").css("display", "none");
 	},
 
+	showVisualizeSubitems = function () {
+		$(".visualize-subitem").css("display", "block");
+	}
+
+	hideVisualizeSubitems = function () {
+		$(".visualize-subitem").css("display", "none");
+	}
+
 	unselectMenuItems = function () {
-		$("#sidebar-upload-button, #sidebar-settings-button, #sidebar-ctfg-button, #sidebar-summary-button, #sidebar-cirrus-button, #sidebar-bubblelines-button, #sidebar-custom-button, #sidebar-export-button, #sidebar-help-button").css("background-color", "#3B3B3C");
+		//Hauptmenü-Punkte
+		$("#sidebar-custom-button, #sidebar-export-button, #sidebar-help-button").css("background-color", "#3B3B3C");
+		//Corpus-Punkte
+		$("#sidebar-upload-button, #sidebar-settings-button").css("background-color", "#3B3B3C");
+		//Analyze-Punkte, Visualize-Punkte
+		$(".analyze-subitems, .visualize-subitem").css("background-color", "#3B3B3C");
 	},
 
 	appendContent = function() {
