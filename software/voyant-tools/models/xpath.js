730e4b5da85844e8ce1bf54538e86a31fb9814b9
fabihoot <fabian.huth@stud.uni-regensburg.de>
2015-03-28 17:41:01 +0100

    added xpath parsing and client-server sending

---
+++ software/voyant-tools/models/xpath.js
@@ -0,0 +1,43 @@
+var fs = require('fs'),
+    xml2js = require('xml2js'),
+    xpath = require('xpath'),
+    dom = require('xmldom').DOMParser,
+    filePath = './public/files/'
+    expressions = {};
+    
+
+init = function(fileName, xpathExpressions){
+	parseFile(fileName);
+	expressions = xpathExpressions;
+}
+
+parseFile = function(fileName){
+	var parser = new xml2js.Parser();
+	fs.readFile(filePath + fileName, function(err, data) {
+
+	    parser.parseString(data, function (err, xml) {
+	        console.dir(xml);
+	        console.log('Done parsing...');
+
+	        applyXPath(xml);
+
+	    });
+
+	});
+}
+
+applyXPath = function(xml){
+	for(var expression in expressions) {
+
+  		console.log(k, expressions[k]);
+  		
+    	var doc = new dom().parseFromString(xml);
+    	var title = xpath.select(expression, doc).toString();
+    	console.log(title)
+
+	}
+}
+
+exports.init = init;
+
+
--- software/voyant-tools/public/javascripts/controller/SettingsController.js
+++ software/voyant-tools/public/javascripts/controller/SettingsController.js
@@ -7,23 +7,55 @@ Voyant.SettingsController = (function() {
 		console.log("init SettingsController");		
 	},
 
+	initButtons = function(){
+		var buttonNext = $("#button-next");
+
+		buttonNext.on('click', function(){
+			console.log('button next clicked');
+			getXPathExpressions();
+		});
+	},
+
 	setFileParameter = function(filpath, files){
 		filePath = filepath;
-		files = files
+		files = files;
 	},
 
 	getStopWordListValue = function(){
 		var stopwordValue = $('#stopword-dropdown').val();
-		console.log("Stopwordlist" + stopwordValue);
+		console.log("Stopwordlist: " + stopwordValue);
 	},
 
 	getXPathExpressions = function(){
-		var xPathContent 	= $("xpath-content").val();
-		var xPathAuthor 	= $("xpath-author").val();
-		var xPathTitle 		= $("xpath-title").val();
-		var xPathDocuments 	= $("xpath-documents").val();
+		var xPathContent 	= $("#xpath-content").val();
+		var xPathAuthor 	= $("#xpath-author").val();
+		var xPathTitle 		= $("#xpath-title").val();
+		var xPathDocuments 	= $("#xpath-documents").val();
 
-		
+		if(files.length > 0){
+
+			var xPath = {
+				file_name		: files[0].name,
+				xpath_content	: xPathContent,
+				xpath_author	: xPathAuthor,
+				xpath_title		: xPathTitle,
+				xpath_documents : xPathDocuments
+			}		
+	
+			sendFileToServer(xPath);	
+		} 
+	},
+	
+	sendFileToServer = function(xPath){
+
+		$.post('/xpath/', {data : xPath}, function (result){
+			onXPathReady(result);
+		});
+
+	},
+
+	onXPathReady = function(result){
+
 	};
 
 
