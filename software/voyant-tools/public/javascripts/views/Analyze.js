f0e7782a3a98ef160d059503d057ea2d713b30af
Dominik <domii.bauer@gmail.com>
2015-04-03 18:42:52 +0200

    iframes bei analyze erstellt

--- software/voyant-tools/public/javascripts/views/Analyze.js
+++ software/voyant-tools/public/javascripts/views/Analyze.js
@@ -11,12 +11,29 @@ Voyant.Analyze = (function() {
 		var analyzeTemplate = _.template($("#analyze-tpl").html());
 		$("#content").html(analyzeTemplate);
 		getSelectedTools(); 
+		appendSelectedTools (); 
 	},
 
 	getSelectedTools = function () {
 		selectedTools = Voyant.MainController.getToolList(); 
-		console.log(selectedTools);
 	}, 
+
+	appendSelectedTools = function () {
+		for (var i = 0 ; i < selectedTools.length; i++) {
+			appendHtml(i); 
+			var url = "http://voyant-tools.org/tool/"+selectedTools[i]+"/?input=http://digitalhumanities.org:8080/dhq/vol/3/3/000067.xml"; 
+			$("#iframe"+i).attr('src', url);
+		}	
+	},
+
+	appendHtml = function (i) {
+		$("#analyze-container").append("<div id=tool"+i+" class='analyze-tool-container'><iframe id=iframe"+i+" class=iframe sandbox='allow-same-origin allow-scripts allow-forms'></iframe></div>");
+	}
+
+	getIframeSrc = function () {
+		var src; 
+		return src; 
+	}
 	
 
 	that.init = init;
